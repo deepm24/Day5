d5cd4236b63fd7b9b12a26372b1e50f81e55d1d6
deepm24 <deep60640@gmail.com>
2023-02-05 18:00:55 +0530

    added:day5 work

---
+++ Random1.sh
@@ -0,0 +1,5 @@
+#!bin/bash -X
+#use of RENDOM Function to get single digit
+
+ran=$((RANDOM%10))
+echo $ran
---
+++ unitConv5a.sh
@@ -0,0 +1,10 @@
+#!bin/bash -x
+ #unit convertion inch to feet
+
+echo "Enter length (inches) : "
+read inches 
+
+feet=$((inches/12))
+
+
+echo "Total feet is $feet
